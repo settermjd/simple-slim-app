e3b782fada5a0469a5e2bedf2657802e6a98c5d3
Matthew Setter <matthew@matthewsetter.com>
2025-11-20 10:39:48 +1000

    Add a route to test sending emails directly
    I've been having a number of issues with the various GitHub Actions that
    I've been trying to use. So, this change adds a route that has PHP code
    to test (locally) if I can send with SendGrid or not, such as whether
    I'm using the wrong region of an API key that lacks the required
    permissions.

--- public/index.php
+++ public/index.php
@@ -2,17 +2,81 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use SendGrid\Mail\Mail;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$environment = $_ENV["ENVIRONMENT"] ?? "production";
+if ($environment === "development") {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+    $dotenv->required([
+      'RECIPIENT_EMAIL',
+      'SENDER_EMAIL'
+    ]);
+}
+
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+$app->get('/hello/{name}', function (
+    Request $request,
+    Response $response,
+    array $args
+) {
     $name = $args['name'];
     $response
         ->getBody()
         ->write(sprintf("Hello, %s.", ucfirst($name)));
+    return $response;
+});
+
+$app->get('/send-email', function (
+    Request $request,
+    Response $response,
+    array $args
+) {
+    $email = new Mail();
+
+    if (
+        ! array_key_exists('SENDER_EMAIL', $_ENV)
+        || ! array_key_exists('RECIPIENT_EMAIL', $_ENV)
+        || ! array_key_exists('SENDGRID_API_KEY', $_ENV)
+    ) {
+        $response->getBody()->write("An environment variable is missing or has not been set.");
+        return $response->withStatus(400);
+    }
+
+    $email->setFrom($_ENV['SENDER_EMAIL']);
+    $email->setSubject('Sending with Twilio SendGrid is Fun');
+    // Replace the email address and name with your recipient
+    $email->addTo($_ENV['RECIPIENT_EMAIL']);
+    $email->addContent(
+        'text/html',
+        '<strong>and fast with the PHP SDK.</strong>'
+    );
+    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+    try {
+        $sendGridResponse = $sendgrid->send($email);
+        $response
+            ->getBody()
+            ->write(sprintf("Response status: %d\n\n", $sendGridResponse->statusCode()));
+
+        $headers = array_filter($sendGridResponse->headers());
+        $response
+            ->getBody()
+            ->write(sprintf("Response Headers\n\n"));
+        foreach ($headers as $header) {
+            $response
+                ->getBody()
+                ->write('- ' . $header . "\n");
+        }
+    } catch (Exception $e) {
+        $response
+            ->getBody()
+            ->write('Caught exception: ' . $e->getMessage() . "\n");
+    }
+
     return $response;
 });
 
